8d6215e67800806ad5f762653c1af86199e9742e
AshrafElManDoh <ashrafelmandoh77@gmail.com>
2025-12-31 16:50:10 +0200

    add bloc provider and service locator

---
+++ lib/core/base_fuctions/get_it.dart
@@ -0,0 +1,20 @@
+import 'package:clean_arch_bookly_app/core/network/api_service.dart';
+import 'package:clean_arch_bookly_app/features/home/data/data_sources/home_local_data_source.dart';
+import 'package:clean_arch_bookly_app/features/home/data/data_sources/home_remote_data_source.dart';
+import 'package:clean_arch_bookly_app/features/home/data/repos/home_repo_imp.dart';
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+
+void setupServiceLocator() {
+  getIt.registerSingleton<ApiService>(ApiService(Dio()));
+  getIt.registerSingleton<HomeRepoImp>(
+    HomeRepoImp(
+      homeLocalDataSource: HomeLocalDataSourceImpl(),
+      homeRemoteDataSource: HomeRemoteDataSourceImpl(
+        apiService: getIt.get<ApiService>(),
+      ),
+    ),
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:clean_arch_bookly_app/core/base_fuctions/get_it.dart';
 import 'package:clean_arch_bookly_app/core/constants/constants.dart';
 import 'package:clean_arch_bookly_app/core/router/app_router.dart';
 import 'package:clean_arch_bookly_app/core/themes/app_themes.dart';
+import 'package:clean_arch_bookly_app/features/home/data/repos/home_repo_imp.dart';
 import 'package:clean_arch_bookly_app/features/home/domain/entities/book_entity.dart';
+import 'package:clean_arch_bookly_app/features/home/domain/use_cases/fetch_featured_books_use_case.dart';
+import 'package:clean_arch_bookly_app/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
 
 void main() async {
@@ -13,6 +18,7 @@ void main() async {
   Hive.registerAdapter(BookEntityAdapter());
   await Hive.openBox<BookEntity>(kOpenFeaturedBox);
   await Hive.openBox<BookEntity>(kOpenNewestBox);
+  setupServiceLocator();
   runApp(const MyApp());
 }
 
@@ -21,12 +27,21 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: AppRouter.router,
-      debugShowCheckedModeBanner: false,
-      darkTheme: AppThemes.darkTheme,
-      theme: AppThemes.lightTheme,
-      themeMode: ThemeMode.system,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => FeaturedBooksCubit(
+            FetchFeaturedBooksUseCase(homeRepo: getIt.get<HomeRepoImp>()),
+          ),
+        ),
+      ],
+      child: MaterialApp.router(
+        routerConfig: AppRouter.router,
+        debugShowCheckedModeBanner: false,
+        darkTheme: AppThemes.darkTheme,
+        theme: AppThemes.lightTheme,
+        themeMode: ThemeMode.system,
+      ),
     );
   }
 }
